87ded164e51ac141c0b78be3c12be46eb5099bdc
Ubuntu <xthecutter@azure-task2.vhxoyaiyxttufepc000wkvfjwe.zx.internal.cloudapp.net>
2024-08-20 12:00:53 +0000

    Solution

--- app/start.sh
+++ app/start.sh
@@ -1,8 +1,8 @@
 #! /bin/bash 
 
 echo "Running a system check..."
-lsblk -o NAME,HCTL,SIZE,MOUNTPOINT > /data/app/todolist/static/files/task3.log
+lsblk -o NAME,HCTL,SIZE,MOUNTPOINT > /data/azure_task_3_attach_data_disk/app/todolist/static/files/task3.log
 
-pip install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8080
+pip install -r /data/azure_task_3_attach_data_disk/app/requirements.txt
+python3 /data/azure_task_3_attach_data_disk/app/manage.py migrate
+python3 /data/azure_task_3_attach_data_disk/app/manage.py runserver 0.0.0.0:8080
